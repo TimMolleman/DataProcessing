cdd9b5394341359211482aed10112ae17bde043b
TimMolleman <timmolleman@hotmail.com>
2016-11-14 17:41:22 +0100

    SVG1 commit

---
+++ Homework/SVG1/main1.js
@@ -0,0 +1,14 @@
+/* use this to test out your function */
+window.onload = function() {
+ 	changeColor("no", "#f4a460");
+ 	changeColor("lv", "#ffdd00");
+ 	changeColor("hu", "#ff00ff");
+ 	changeColor("it", "#00ffdd");
+
+}
+/* changeColor takes a path ID and a color (hex value)
+   and changes that path's fill color */
+function changeColor(id, color) {
+	var svg = document.getElementById(id);
+	svg.style.fill = color;
+};
